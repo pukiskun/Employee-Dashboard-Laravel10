0800ac5148b335b9fc1e9375319d734a48f76f62
pukiskun <fatalitycloud@gmail.com>
2023-05-19 07:29:58 +0700

    commited

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,12 +13,10 @@ initial-scale=1.0">
 <body>
     <div class="container text-center my-5">
         <h1 class="mb-4">Belajar Controller & View pada Laravel</h1>
-        {{-- Contoh cara mereferensikan gambar di dalam file blade dengan
-menggunakan pendekatan Vite --}}
         <img class="img-thumbnail" src="{{ Vite::asset('resources/images/laravel.png') }}" alt="image">
         <div class="col-md-2 offset-md-5 mt-4">
             <div class="d-grid gap-2">
-                <a class="btn btn-dark" href="">Home</a>
+                <a class="btn btn-dark" href="{{ route('home') }}">Home</a> {{-- Menghubungkan button home ke halaman home --}}
             </div>
         </div>
     </div>
